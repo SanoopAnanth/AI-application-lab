618b0483effe41712999fbe1299df9a2ab8a406a
SanoopAnanth <sanoopananth2003@gmail.com>
2023-11-20 19:36:49 +0530

    ....

--- lab12b.py
+++ lab12b.py
@@ -4,7 +4,6 @@ from nltk.corpus import movie_reviews
 from nltk.tokenize import word_tokenize
 from nltk.classify.scikitlearn import SklearnClassifier
 from sklearn.naive_bayes import MultinomialNB
-from sklearn.svm import SVC
 
 # nltk.download('movie_reviews')
 # nltk.download('punkt')
